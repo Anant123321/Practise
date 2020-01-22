d5a16e5bc985f36105c818aab30bd7c04e08246d
Anant <48465662+Anant123321@users.noreply.github.com>
2020-01-22 22:24:56 +0530

    Create Four sum.java
    Given an array A of N integers, find a combination of four elements in the array whose sum is equal to a given value X.
    
    Input:
    First line consists of T test cases. First line of every test case consists of an integer N, denoting the number of elements in an array. Second line consists of N spaced array elements. Third line of every test case X.
    
    Output:
    Single line output, print 1 if combination is found else 0.
    
    Constraints:
    1 <= T <= 100
    1 <= N <= 106
    
    Example:
    Input:
    1
    6
    1 5 1 0 6 0
    7
    Output:
    1
    
    Explantion:
    Testcase 1: 1, 5, 1, 0 are the four elements which makes sum 7.

---
+++ Four sum.java
@@ -0,0 +1,51 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static int check(int []arr, int target)
+    {
+        int sum=0,len=arr.length;
+        for(int i=0,j=len-1;i<j-2;)
+        {
+            int l = i+1;
+            int r = j-1;
+            while(l<r)
+            {
+                int tempSum = arr[l] + arr[r] + arr[i] + arr[j];
+                //System.out.println(arr[i]+" "+arr[l]+" "+arr[r]+" "+arr[j]+" "+tempSum);
+                if(tempSum==target)return 1;
+                if(tempSum<target)l++;
+                else
+                {
+                    r--;
+                }
+                sum=tempSum;
+            }
+            if(sum<target)i++;
+            else
+            {
+                j--;
+            }
+        }
+        return 0;
+    }
+	public static void main (String[] args) {
+	    Scanner scan= new Scanner(System.in);
+	    int testCases = scan.nextInt();
+	    for(int z=0;z<testCases;z++)
+	    {
+	        int size = scan.nextInt();
+	        int arr[] = new int[size];
+	        for(int p=0;p<size;p++)
+	        {
+	            arr[p] = scan.nextInt();
+	        }
+	        int target = scan.nextInt(); 
+	        Arrays.sort(arr);
+	        System.out.println(check(arr,target));
+	    }
+		
+	}
+}
